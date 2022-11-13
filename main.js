3114d0f5d9f317b7de6d792f3a42a1b7e26a5d02
zekothedev <z.erassan@gmail.com>
2022-11-13 02:09:38 +0100

    Create API and include first two cities

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 const express = require('express')
 const app = express()
+const PORT = 8000
+
+const capitalCities = {
+
+    'amsterdam': {
+      'Mayor': 'Femke Halsema',
+      'Country': 'The Netherlands',
+      'Area': '219.3 km²'
+    },
+
+    'paris': {
+      'Mayor': 'Anne Hidalgo',
+      'Country': 'France',
+      'Area': '105.4 km²'
+    },
+
+    'unknown': {
+      'Mayor': 'Unknown',
+      'Country': 'Unknown',
+      'Area': 'Unknown'
+    }
+}
 
 app.get('/', (request, response) => {
-  
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:capital', (request, response) => {
+  const chosenCapital = request.params.capital.toLowerCase()
+  if (capitalCities[chosenCapital]){
+    response.json(capitalCities[chosenCapital])
+  }else {
+    response.json(capitalCities['unknown'])
+  };
+})
+
+app.listen(PORT, () => {
+  console.log(`The server is now running on port ${PORT}`)
 })
