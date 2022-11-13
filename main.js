d773778ed54562319d7c7a036273537449d02671
zekothedev <z.erassan@gmail.com>
2022-11-13 21:54:06 +0100

    Add more mayor capitals and their data

--- main.js
+++ main.js
@@ -16,6 +16,66 @@ const capitalCities = {
       'Area': '105.4 km²'
     },
 
+    'brussels': {
+      'Mayor': 'Philippe Close',
+      'Country': 'Belgium',
+      'Area': '32.61 km²'
+    },
+
+    'sydney': {
+      'Mayor': 'Clover Moore',
+      'Country': 'Australia',
+      'Area': '12,368 km²'
+    },
+
+    'berlin': {
+      'Mayor': 'Franziska Giffey',
+      'Country': 'Germany',
+      'Area': '891.8 km²'
+    },
+
+    'copenhagen': {
+      'Mayor': 'Frank Jensen',
+      'Country': 'Denmark',
+      'Area': '179.8 km²'
+    },
+
+    'dublin': {
+      'Mayor': 'Paul McAuliffe',
+      'Country': 'Ireland',
+      'Area': '117.8 km²'
+    },
+
+    'london': {
+      'Mayor': 'Sadiq Khan',
+      'Country': 'United Kingdom',
+      'Area': '1,572 km²'
+    },
+
+    'rabat': {
+      'Mayor': 'Asmaa Rhlalou',
+      'Country': 'Morocco',
+      'Area': '117 km²'
+    },
+
+    'washington': {
+      'Mayor': 'Muriel Bowser',
+      'Country': 'United States',
+      'Area': '177 km²'
+    },
+
+    'abu dhabi': {
+      'Mayor': 'Khalifa bin Zayed Al Nahyan',
+      'Country': 'United Arab Emirates',
+      'Area': '972 km²'
+    },
+
+    'rome': {
+      'Mayor': 'Roberto Gualtieri',
+      'Country': 'Italy',
+      'Area': '1,285 km²'
+    },
+
     'unknown': {
       'Mayor': 'Unknown',
       'Country': 'Unknown',
